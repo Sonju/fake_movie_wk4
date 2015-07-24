c2789887a974c22bdd01abf38def6b89e9a4a355
Sonju Walker <sonjuwalker@gmail.com>
2015-07-24 15:15:36 -0500

    fake_movie updated

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'bundler'
+Bundler.require()
+
+
+class fake_movie
+ def initialize = (name, release_year, descprition)
+    @name = name
+    @
+  end
+
+  def name
+    @name
+  end
+
+  def release_year
+    @release_year
+  end
+
+  def descprition
+    @descprition
+  end
+
+  def .to_s
+    puts name + ' was released in ' + release_year + '.' + name + "is about " + descprition + '.'
+  end
+
+  def '/api/Spiderman'
+    puts Spiderman = fake_movie.new ('Spiderman', '1970', 'a spider bites a boy and he becomes a superhero').to_s
+  end
+
+end
---
+++ config.ru
@@ -0,0 +1,4 @@
+require './app'
+
+run sinatra-cross_origin::Application
+	
